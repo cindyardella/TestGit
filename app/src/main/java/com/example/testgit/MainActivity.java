4cac5d2d96f540ba7f76f146d9345455d4d6a03a
cindyardella <cindyardella02@gmail.com>
2021-10-04 17:12:38 +0700

    Splash Screen dan Bottom Navigation Menu

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.testgit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment f = null;
+            switch (item.getItemId()){
+                case R.id.menu_home:
+                    f = new HomeFragment();
+                    break;
+                case R.id.menu_hotel:
+                    f = new HotelFragment();
+                    break;
+                case R.id.menu_profile:
+                    f = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
     }
 }
