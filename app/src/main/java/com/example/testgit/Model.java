f599ea34b2255b4c00c2a03ced9ba761414d4415
cindyardella <cindyardella02@gmail.com>
2021-10-24 19:28:18 +0700

    Dummy data RecyclerView

---
+++ app/src/main/java/com/example/testgit/Model.java
@@ -0,0 +1,24 @@
+package com.example.testgit;
+
+public class Model {
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(int gambar) {
+        this.gambar = gambar;
+    }
+
+    private String nama;
+    private int gambar;
+
+}
