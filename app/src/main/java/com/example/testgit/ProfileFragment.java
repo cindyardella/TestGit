2abf7d4888351f59dda509bc8d34d9afb3add33c
RinaYulianaPutri <RinaYulianaPutri@gmail.com>
2021-11-28 21:02:53 +0700

    Profile Account

--- app/src/main/java/com/example/testgit/ProfileFragment.java
+++ app/src/main/java/com/example/testgit/ProfileFragment.java
@@ -1,19 +1,24 @@
 package com.example.testgit;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import android.app.AlertDialog; import android.content.DialogInterface; import android.os.Bundle; import android.view.LayoutInflater; import android.view.View; import android.view.ViewGroup; import android.widget.Button;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable; import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+    @Nullable     @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
+            ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+        Button submit = view.findViewById(R.id.submit);         MainActivity act = (MainActivity) getActivity();         submit.setOnClickListener(v -> {
+            android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(requireContext());             alertDialogBuilder.setTitle("Login");             alertDialogBuilder
+                    .setMessage("Login Successfull")
+                    .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
 
-        }
+                            act.bottomNavigationView.setSelectedItemId(R.id.menu_home);
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();             alertDialog.show();
+        });
+        return view;
+    }
 }
